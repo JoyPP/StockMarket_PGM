6ebfd7994342969a18eb390a92f9b0a43ab93f43
JoyPP <piaozai88@163.com>
2018-01-24 15:34:39 +0800

    fix bugs joint-all model

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ def get_bullish_set(dataset, svm_model, threshold = 0.5):
     '''
     x_train, y_train = dataset
     y_pred = svm_model.predict_proba(x_train)
-    bullish_idx = np.where(svm_model.classes_)[0][0]
+    bullish_idx = np.where(svm_model.classes_>0)[0][0]
     y_pred = y_pred[:, bullish_idx]
 
     idx = np.where(y_pred >= threshold)[0]
@@ -24,11 +24,18 @@ def get_bullish_set(dataset, svm_model, threshold = 0.5):
 
 
 def ChiSquareTest(c_u, i_u, c_bl, i_bl):
+    '''
+    return the chi square test statistic
+    '''
     test_stat = math.pow(c_u - c_bl, 2) / c_bl + math.pow(i_u - i_bl, 2) / i_bl
     return test_stat
 
 
 def ExpertPValure(dataset, svm_model, p_bl, alpha, threshold = 0.5):
+    '''
+    dataset is one user's dataset
+    test whether user is expert and his p_value if he is
+    '''
     # get bullish dataset
     x_train, _ = get_bullish_set(dataset, svm_model, threshold)
 
@@ -67,7 +74,7 @@ def Best_Threshold(dataset, svm_model, p_bl, alpha):
 
 
 
-class Join_All:
+class Joint_All:
     '''
     learn a single SVM from training data, apply it to test dataset, rank tweets according to the SVM score
     '''
@@ -75,15 +82,28 @@ class Join_All:
         self.train_data = train_data
         self.hold_data = hold_data
         self.test_data = test_data
-        self.clf = SVC()
+        self.clf = SVC(probability=True)
 
     def train(self):
-        x_train, y_train = self.train_data
-        self.clf.fit(x_train, y_train)
-
-    def test(self):
-        x_test, y_test = self.test_data
-        score = self.clf.score(x_test, y_test)
+        '''
+        return the ranking list and baseline precision
+        '''
+        # learning SVM from training data
+        for userid, (x_u, y_u) in zip(self.train_data):
+            # counting the number of positive examples in training data
+            c_bl = len(np.where(np.array(y_u)>0)[0])
+            total = len(y_u)
+            self.clf.fit(x_u, y_u)
+        p_bl = float(c_bl) / total
+        # apply SVM to test data
+        rank_list = list()  # list of (x_u, y_u, y_pred)
+        for userid, (x_u, y_u) in zip(self.test_data):
+            y_pred = self.clf.predict_proba(x_u)
+            bullish_idx = np.where(self.clf.classes_>0)[0][0]
+            y_pred = y_pred[:, bullish_idx]
+            rank_list.extend(map(lambda a,b,c: (a,b,c), x_u, y_u, y_pred))
+            rank_list.sort(key=lambda tup: tup[2], reverse=True)
+        return rank_list, p_bl
 
 
 class Per_User:
@@ -96,7 +116,7 @@ class Per_User:
         self.hold_data = hold_data
         self.test_data = test_data
 
-    def train(self):
+    def train(self, p_bl, alpha):
         '''
         get a dict of experts.  {userid: {"threshold": t, "p_value": p_value}}
         '''
@@ -109,7 +129,7 @@ class Per_User:
                 clf = SVC(probability=True)
                 clf.fit(x_train, y_train)
                 # optimize the classification threshold (grid search) resulting in best p-value for each classifier in hold-out dataset
-                best_threshold, p_value = Best_Threshold(self.hold_data, clf, p_lb, alpha)
+                best_threshold, p_value = Best_Threshold(self.hold_data, clf, p_bl, alpha)
                 if best_threshold is not None:
                     self.expert_id.append(userid)
                     p_list.append(p_value)
@@ -129,21 +149,22 @@ class Joint_Experts:
         self.train_data = train_data
         self.hold_data = hold_data
         self.test_data = test_data
+        self.clf = SVC(probability=True)
 
-    def train(self):
+    def train(self, p_bl, alpha):
         per_user = Per_User(self.train_data, self.hold_data, self.test_data)
-        per_user.train()
+        per_user.train(p_bl, alpha)
         self.expert_id = per_user.expert_id
-        clf = SVC()
+
         score_list = list()
         rank_list = list()
         for userid in self.expert_id:
             x, y = self.train_data[userid]
-            clf.fit(x, y)
+            self.clf.fit(x, y)
         for userid in self.expert_id:
-            data_u = get_bullish_set(self.test_data[userid], clf)
+            data_u = get_bullish_set(self.test_data[userid], self.clf)
             x_u, y_u = data_u
-            score_u = clf.score(x_u, y_u)
+            score_u = self.clf.score(x_u, y_u)
             score_list.append(score_u)
             rank_list.append(data_u)
         rank_list = list(np.take(rank_list, np.argsort(score_list)))
